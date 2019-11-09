867108bcb425220e95c4ead709a3b9a4b6961efc
Yousef Akbar <yousef_akbar@me.com>
2019-11-09 14:29:13 -0800

    improved commands and key bindings; super key used

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SauceCodePro Nerd Font:size=11" };
-static const char dmenufont[]       = "Inconsolata-Bold:size=14";
+static const char *fonts[]          = { "SauceCodePro Nerd Font:size=10" };
+static const char dmenufont[]       = "Inconsolata-Bold:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -74,20 +74,26 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *qutebrowsercmd[]  = { "qutebrowser", NULL };
-static const char *filemanagercmd[]  = { "st", "-e", "ranger", NULL };
-static const char *neomuttcmd[]  = { "st", "-e", "neomutt", NULL };
-static const char *lockcmd[]  = { "i3lock-fancy", NULL };
+static const char *cmd_term[]  = { "st", NULL };
+static const char *cmd_browser[]  = { "qutebrowser", NULL };
+static const char *cmd_fileman[]  = { "st", "-e", "ranger", NULL };
+static const char *cmd_mutt[]  = { "st", "-e", "neomutt", NULL };
+static const char *cmd_lock[]  = { "i3lock-fancy", NULL };
+static const char *cmd_passmenu[] = { "passmenu", NULL };
+static const char *cmd_manmenu[] = { "manmenu", NULL };
+static const char *cmd_shutdown[] = { "shutdown.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ Mod4Mask,              	XK_0,	   spawn,          {.v = lockcmd } },
-	{ Mod4Mask,              	XK_1,	   spawn,          {.v = qutebrowsercmd } },
-	{ Mod4Mask,              	XK_2,	   spawn,          {.v = filemanagercmd } },
-	{ Mod4Mask,              	XK_3,	   spawn,          {.v = neomuttcmd } },
+	{ Mod4Mask,                     XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = cmd_term } },
+	{ Mod4Mask,              	XK_l,	   spawn,          {.v = cmd_lock } },
+	{ Mod4Mask,              	XK_1,	   spawn,          {.v = cmd_browser } },
+	{ Mod4Mask,              	XK_2,	   spawn,          {.v = cmd_fileman } },
+	{ Mod4Mask,              	XK_3,	   spawn,          {.v = cmd_mutt } },
+	{ Mod4Mask,			XK_p,	   spawn,	   {.v = cmd_passmenu} },
+	{ Mod4Mask,			XK_m,	   spawn,	   {.v = cmd_manmenu} },
+	{ Mod4Mask,			XK_x,	   spawn,	   {.v = cmd_shutdown} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -131,7 +137,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = cmd_term } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
