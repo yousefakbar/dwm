aba16e551b0ca78b5d6125b78fef00062ad4f9e2
Yousef Akbar <yousef_akbar@me.com>
2019-12-10 20:19:15 -0800

    removed redundant comments from prev patching

--- dwm.c
+++ dwm.c
@@ -845,18 +845,6 @@ drawbar(Monitor *m)
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
-
-// 	if ((w = m->ww - sw - x) > bh) {
-// 		if (m->sel) {
-//+			int mid = (m->ww - TEXTW(m->sel->name)) / 2 - x;
-//+			/* make sure name will not overlap on tags even when it is very long */
-//+			mid = mid >= lrpad / 2 ? mid : lrpad / 2;
-// 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-//-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
-//+			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
-
-
-
 	if ((w = m->ww - sw - stw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
